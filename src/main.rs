fe164cf3ab556a20b7c1a20ad2c52dfe1e39ae12
William Myers <mwilliammyers@gmail.com>
2018-10-28 13:34:09 -0600

    Refactor to use hyper instead of warp

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,67 @@
-use hyper::rt::Future;
 use lazy_static::lazy_static;
 use log::*;
 use regex::Regex;
 use serde_derive::Deserialize;
 // use serde_json;
-use hyper;
+use futures::{future, Future};
+use hyper::{
+    client::HttpConnector, header, service::service_fn, Body, Client, Method, Request, Response,
+    Server, StatusCode,
+};
 use std::{io, io::Write, thread, time::Duration, vec::Vec};
-use warp::{self, path, Filter};
 
 mod button;
 mod config;
 mod fetch;
 mod led;
 
+type BoxFut = Box<Future<Item = Response<Body>, Error = hyper::Error> + Send>;
+
+static NOTFOUND: &[u8] = b"Not Found";
+static PONG: &[u8] = b"Pong";
+
 lazy_static! {
     static ref RE: Regex =
         Regex::new(r"(?i).*blink(?:\s+the)?\s+light\s+for\s+(\d+)\s*ms.*").unwrap();
     static ref CONFIG: config::EnvVars = config::from_env().unwrap();
+}
+
+fn route(req: Request<Body>, _client: &Client<HttpConnector>) -> BoxFut {
+    let (parts, _body) = req.into_parts();
+
+    match (parts.method, parts.uri.path()) {
+        (Method::GET, "/ping") => Box::new(future::ok(Response::new(Body::from(PONG)))),
+        (Method::GET, "/led/configure") => {
+            print!("New configuration? ");
+            io::stdout().flush().unwrap();
+
+            let mut input = String::new();
+            io::stdin().read_line(&mut input).unwrap();
+            debug!("user input: {}", &input);
+
+            let cap = RE.captures(&input).unwrap();
+            debug!("parsed user input: {:?}", &cap);
+
+            let new_led_args = led::BlinkArguments {
+                duration: Duration::from_millis(cap[1].parse().unwrap()),
+                // TODO: get this from the user too
+                period: Duration::from_millis(500),
+            };
+
+            Box::new(future::ok(
+                Response::builder()
+                    .header(header::CONTENT_TYPE, "application/json")
+                    .body(Body::from(serde_json::to_string(&new_led_args).unwrap()))
+                    .unwrap(),
+            ))
+        }
+        _ => Box::new(future::ok(
+            Response::builder()
+                .status(StatusCode::NOT_FOUND)
+                .body(Body::from(NOTFOUND))
+                .unwrap(),
+        )),
+    }
 }
 
 fn main() {
@@ -66,30 +111,14 @@ fn main() {
         });
     }
 
-    let ping = warp::path("ping").map(|| "pong");
+    hyper::rt::run(future::lazy(move || {
+        let client = Client::new();
 
-    let led_configure = path!("led" / "configure").map(|| {
-        print!("New configuration? ");
-        io::stdout().flush().unwrap();
-
-        let mut input = String::new();
-        io::stdin().read_line(&mut input).unwrap();
-        debug!("user input: {}", &input);
-
-        let cap = RE.captures(&input).unwrap();
-        debug!("parsed user input: {:?}", &cap);
-
-        let new_led_args = led::BlinkArguments {
-            duration: Duration::from_millis(cap[1].parse().unwrap()),
-            // TODO: get this from the user too
-            period: Duration::from_millis(500),
-        };
-
-        warp::reply::json(&[new_led_args])
-    });
-
-    let routes = warp::get2()
-        .and(ping.or(led_configure))
-        .or(warp::post2().and(led_configure));
-    warp::serve(routes).run(CONFIG.self_address);
+        info!("listening on http://{}", &CONFIG.self_address);
+        Server::bind(&CONFIG.self_address)
+            .serve(move || {
+                let client = client.clone();
+                service_fn(move |req| route(req, &client))
+            }).map_err(|e| error!("server error: {}", e))
+    }));
 }
