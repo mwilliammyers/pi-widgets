a0a8c5a5a6553e1cba29dfb4a7d7806accd3a20a
William Myers <mwilliammyers@gmail.com>
2018-12-05 00:13:18 -0700

    Add preliminary support for mic recording
    WIP - not working

--- src/gpio.rs
+++ src/gpio.rs
@@ -1,7 +1,13 @@
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::{Arc, Mutex};
+use std::{fs, thread, time::Duration, vec::Vec};
+
 use futures::{Future, Stream};
 use hyper::{self, Body, Client, Request, Uri};
 use log::*;
-use std::{fs, thread, vec::Vec};
+
+use cpal;
+use hound;
 
 pub mod led {
     use std::{
@@ -125,6 +131,7 @@ pub fn init() {
             let led_addr: Uri = CONFIG.led_address.parse().unwrap();
             let display_addr: Uri = CONFIG.display_address.parse().unwrap();
             let led_config = &CONFIG.led_config;
+            let audio_recording = &CONFIG.audio_recording;
 
             if let Some(led_button_line) = CONFIG.led_button_line {
                 if line == led_button_line {
@@ -142,17 +149,86 @@ pub fn init() {
 
             if let Some(display_button_line) = CONFIG.display_button_line {
                 if line == display_button_line {
-                    // TODO: share client from outside thread?
-                    let fut = Client::new()
-                        .get(display_addr)
-                        .and_then(|res| res.into_body().concat2())
-                        .and_then(move |body| {
-                            fs::write(led_config, body).unwrap();
-                            Ok(())
-                        }).map_err(|err| error!("{}", err));
+                    // Setup the default input device and stream with the default input format.
+                    let device = cpal::default_input_device().unwrap();
+                    let format = device.default_input_format().unwrap();
 
-                    // TODO: use existing rt?
-                    hyper::rt::run(fut);
+                    debug!("Default input format: {:?}", format);
+                    let event_loop = cpal::EventLoop::new();
+                    let stream_id = event_loop.build_input_stream(&device, &format).unwrap();
+                    event_loop.play_stream(stream_id);
+
+                    // The WAV file we're recording to.
+                    let writer = hound::WavWriter::create(
+                        &audio_recording,
+                        hound::WavSpec {
+                            channels: format.channels,
+                            sample_rate: format.sample_rate.0,
+                            bits_per_sample: (format.data_type.sample_size() * 8) as u16,
+                            sample_format: hound::SampleFormat::Int,
+                        },
+                    ).unwrap();
+                    let writer = Arc::new(Mutex::new(Some(writer)));
+
+                    // A flag to indicate that recording is in progress.
+                    let recording = Arc::new(AtomicBool::new(true));
+
+                    // Run the input stream on a separate thread.
+                    let writer_2 = writer.clone();
+                    let recording_2 = recording.clone();
+                    thread::spawn(move || {
+                        event_loop.run(move |_, data| {
+                            // If we're done recording, return early.
+                            if !recording_2.load(Ordering::Relaxed) {
+                                return;
+                            }
+                            // Otherwise write to the wav writer.
+                            match data {
+                                cpal::StreamData::Input {
+                                    buffer: cpal::UnknownTypeInputBuffer::U16(buffer),
+                                } => {
+                                    if let Ok(mut guard) = writer_2.try_lock() {
+                                        if let Some(writer) = guard.as_mut() {
+                                            for sample in buffer.iter() {
+                                                let sample = cpal::Sample::to_i16(sample);
+                                                writer.write_sample(sample).ok();
+                                            }
+                                        }
+                                    }
+                                }
+                                cpal::StreamData::Input {
+                                    buffer: cpal::UnknownTypeInputBuffer::I16(buffer),
+                                } => {
+                                    if let Ok(mut guard) = writer_2.try_lock() {
+                                        if let Some(writer) = guard.as_mut() {
+                                            for &sample in buffer.iter() {
+                                                writer.write_sample(sample).ok();
+                                            }
+                                        }
+                                    }
+                                }
+                                cpal::StreamData::Input {
+                                    buffer: cpal::UnknownTypeInputBuffer::F32(buffer),
+                                } => {
+                                    if let Ok(mut guard) = writer_2.try_lock() {
+                                        if let Some(writer) = guard.as_mut() {
+                                            for sample in buffer.iter() {
+                                                let sample = cpal::Sample::to_i16(sample);
+                                                writer.write_sample(sample).ok();
+                                            }
+                                        }
+                                    }
+                                }
+                                _ => (),
+                            }
+                        });
+                    });
+
+                    thread::sleep(Duration::from_secs(7));
+                    recording.store(false, Ordering::Relaxed);
+
+                    writer.lock().unwrap().take().unwrap().finalize().unwrap();
+                    debug!("Recording complete!");
                 }
             }
         }).unwrap();
